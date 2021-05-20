9a50361461f1f630d3e5ac141ec229c272aa38ce
Cristian Gabriel Ulloque <gaby_ulloque@hotmail.com>
2021-05-20 18:03:40 -0300

    Add files via upload

---
+++ js_file.js
@@ -0,0 +1,19 @@
+const typed = new Typed('.typed', {
+  strings: [
+            '',
+            '<i class = "dato">Grande del mundo</i>',
+            '<i class = "dato">Lo juegan 270.000.000 personas</i>',
+            '<i class = "dato">En 194 paises distintos</i>', 
+            ],
+            typeSpeed: 90, 
+            startDelay: 100, 
+            backSpeed: 90, 
+            smartBackspace: true, 
+            backDelay: 1500, 
+            loop: true, 
+            loopCount: false, 
+            showCursor: true, 
+            cursorChar: '|',
+            contentType: 'html', 
+});
+
